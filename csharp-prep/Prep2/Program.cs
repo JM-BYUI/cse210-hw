9e68422ac4e1b57dac54c7104a82c84fb75bdefc
JM-BYUI <johnfranklinmurdock@gmail.com>
2025-09-19 12:53:40 -0600

    letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        string letter = "F";
+        string sign = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        if (letter != "F")
+        {
+            if (number % 10 >= 7 && letter !="A")
+            {
+                sign = "+";
+
+            }
+            else if (number % 10 < 3)
+            {
+                sign = "-";
+
+            }
+        }
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+
     }
 }
