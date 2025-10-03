a76a314ea2ecc43c14bc4184241721cdebcd10e4
JM-BYUI <johnfranklinmurdock@gmail.com>
2025-10-02 20:04:28 -0600

    abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Lays";
+        job1._jobTitle = "Chip Tester";
+        job1._startYear = 1999;
+        job1._endyear = 2004;
+        Job job2 = new Job();
+        job2._company = "PepsiCo";
+        job2._jobTitle = "Soda Tester";
+        job2._startYear = 2005;
+        job2._endyear = 2011;
+        Resume r = new Resume();
+        r._name = "Todd Muncherson";
+        r._jobs.Add(job1);
+        r._jobs.Add(job2);
+        r.Display();
     }
 }
